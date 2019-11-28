15177424d1c2cbeb03cb8f8f5b733133bcaaac62
wenxiaojie <56551200+wenxiaojie@users.noreply.github.com>
2019-11-28 21:51:18 +0800

    Update orderconvert.py

--- orderconvert.py
+++ orderconvert.py
@@ -1,27 +1,26 @@
 # Pythono3 code to rename multiple  
 # files in a directory or folder 
-  
+
 # importing os module 
-import os 
+import os
 import glob
 
 img_list = glob.glob("data/custom/test/*.png")
 
 # Function to rename multiple files 
-def main(): 
-      
-    for index in range(len(img_list)): 
+def main():
+    
+    for index in range(len(img_list)):
+        dst = str.zfill(str(index + 1), 5) + ".png"
+        src = 'data/custom/test/' + str(index + 1) + '.png'
+        dst = 'data/custom/test_image1/' + dst
 
-        dst = str.zfill(str(index+1), 5) + ".png"
-        src = 'data/custom/test/'+ str(index+1) + '.png'
-        dst = 'data/custom/test_image1/'+ dst 
-          
         # rename() function will 
         # rename all the files 
-        os.rename(src, dst) 
-  
-# Driver Code 
-if __name__ == '__main__': 
-      
+        os.rename(src, dst)
+
+    # Driver Code 
+
+if __name__ == '__main__':
     # Calling main() function 
     main() 
